44ca723d052efd674464d1a0cd6da97d5f9f90cf
Tahsin Nurun <nuruntahsin@gmail.com>
2021-04-08 22:15:25 +0600

    008

--- src/Component/Vegetables/Vegetables.js
+++ src/Component/Vegetables/Vegetables.js
@@ -12,11 +12,11 @@ const Vegetables = (props) => {
         <div className="col-md-3 col-sm-12 container" style={{ padding: "50px"}} >
 
             <Link to={`/checkOut/${id}`}>
-                <Card style={{ width: '16rem', height: '30rem', padding: "40px"  }}>
+                <Card style={{ width: '16rem', height: '30rem', padding: "40px", backgroundColor:"Honeydew"  }}>
                     <Card.Img variant="top" src={img} alt="" />
                     <Card.Body>
                         <Card.Text>{name}</Card.Text>
-                        <Card.Text>Price: {price}</Card.Text>
+                        <Card.Text>Price:$ {price}</Card.Text>
                         <Button variant="primary">buy now</Button>
                     </Card.Body>
                 </Card>
